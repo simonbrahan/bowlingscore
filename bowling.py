2051c7df1e5944283b9d38d5e85b2d0609d7354a
Simon Brahan <simon.brahan@team.bumble.com>
2025-07-02 10:43:00 +0100

    Added frame class skeleton. Added scenario naming to tests.

--- bowling.py
+++ bowling.py
@@ -2,13 +2,39 @@ def scoreGame(rolls):
     return sum(rolls)
 
 
-gametests = [
-    [[0,0,0,0,0,0,0,0,0,0], 0],
-    [[9,9,9,9,9,9,9,9,9,9], 90]
+class Frame:
+    def __init__(self):
+        self.rolls = []
+
+    def addRoll(self, roll):
+        self.rolls.append(roll)
+
+    def score(self):
+        return sum(self.rolls)
+
+
+frametests = [
+    ['bad frame', [0,0], 0]
 ]
 
-for rolls, expectedScore in gametests:
+for scenario, rolls, expectedScore in frametests:
+    frame = Frame()
+    for roll in rolls:
+        frame.addRoll(roll)
+
+    actualScore = frame.score()
+    if actualScore != expectedScore:
+        print('scenario: "', scenario, '" scored', actualScore, 'but expected', expectedScore)
+
+
+gametests = [
+    ['bad game', [0,0,0,0,0,0,0,0,0,0], 0],
+    ['ok game', [9,9,9,9,9,9,9,9,9,9], 90],
+    # ['score a spare', [9,1,5,0,0,0,0,0,0,0], 20],
+]
+
+for scenario, rolls, expectedScore in gametests:
     actualScore = scoreGame(rolls)
     if actualScore != expectedScore:
-        print('rolls:', rolls, 'scored', actualScore, 'but expected', expectedScore)
+        print('scenario: "', scenario, '" scored', actualScore, 'but expected', expectedScore)
 
